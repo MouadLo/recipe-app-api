d35084979578ab06cddc45fb6596570523076f1c
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2022-02-23 07:50:12 +0100

    Add support for creating superusers

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -15,3 +15,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+      """Test the email for a new user is normalized"""
+      email = 'test@ANFADev.COM'
+      user = get_user_model().objects.create_user(email,'test1234')
+      self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a  new superuser"""
+        user = get_user_model().objects.create_superuser(
+          'test@anfadev.com',
+          'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
